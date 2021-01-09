7311508902d450556555e92f33235e049c203ebc
Cristian Morales Jaureguiberry <cjmorales@colbunsa.cl>
2021-01-09 19:33:22 -0300

    version 3

---
+++ cars/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Car
+# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+	list_display =('car_tittle','color','year','body_style','fuel_type','is_featured')
+
+
+	list_editable = ('is_featured',)
+	search_fields = ('car_tittle','color','year','body_style','body_style',)
+
+admin.site.register(Car, CarAdmin)
